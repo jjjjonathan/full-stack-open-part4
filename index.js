dd6c3b88601eff3f3fee20dfedc6ee292bd17d8f
jjjjonathan <hornathanjon@gmail.com>
2021-04-03 18:44:17 -0500

    add config util

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-require('dotenv').config();
 const http = require('http');
 const express = require('express');
 const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
+const config = require('./utils/config');
 const logger = require('./utils/logger');
 
 const blogSchema = new mongoose.Schema({
@@ -44,7 +44,6 @@ app.post('/api/blogs', (request, response) => {
   });
 });
 
-const PORT = 3003;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`);
 });
