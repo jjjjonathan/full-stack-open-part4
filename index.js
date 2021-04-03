d5400d953ddd5405d6548f56a973576c80c2c5ac
jjjjonathan <hornathanjon@gmail.com>
2021-04-03 18:27:32 -0500

    exercise 4.1 complete

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const http = require('http');
 const express = require('express');
 const app = express();
@@ -13,13 +14,17 @@ const blogSchema = new mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema);
 
-const mongoUrl = 'mongodb://localhost/bloglist';
-mongoose.connect(mongoUrl, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true,
-});
+const mongoUrl = process.env.MONGODB_URI;
+mongoose
+  .connect(mongoUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  });
 
 app.use(cors());
 app.use(express.json());
