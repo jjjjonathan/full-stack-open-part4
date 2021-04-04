7e197aabd10273dd38180f697692af27cd93c189
jjjjonathan <hornathanjon@gmail.com>
2021-04-03 23:15:43 -0500

    add test helper

--- tests/api.test.js
+++ tests/api.test.js
@@ -1,15 +1,30 @@
 const mongoose = require('mongoose');
 const supertest = require('supertest');
+const helper = require('./test_helper');
 const app = require('../app');
-
 const api = supertest(app);
 
-test('notes are returned as json', async () => {
-  await api
+const Blog = require('../models/blog');
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+  const blogObjects = helper.initialBlogs.map((blog) => new Blog(blog));
+  const promiseArray = blogObjects.map((blog) => blog.save());
+  await Promise.all(promiseArray);
+});
+
+/********** TESTS BELOW **********/
+
+test('correct number of notes are returned as json', async () => {
+  const response = await api
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/);
+
+  expect(response.body).toHaveLength(helper.initialBlogs.length);
 });
+
+/********** TESTS ABOVE **********/
 
 afterAll(() => {
   mongoose.connection.close();
