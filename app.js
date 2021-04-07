f8c4aedfbebe5d6ace46a58bee00ca8f6f0b6d1b
jjjjonathan <hornathanjon@gmail.com>
2021-04-06 19:50:49 -0500

    exercise 4.18 complete

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 const cors = require('cors');
 const blogsRouter = require('./controllers/blogs');
 const usersRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
 const middleware = require('./utils/middleware');
 const logger = require('./utils/logger');
 const mongoose = require('mongoose');
@@ -32,6 +33,7 @@ if (process.env.NODE_ENV !== 'test') app.use(morgan('tiny'));
 
 app.use('/api/blogs', blogsRouter);
 app.use('/api/users', usersRouter);
+app.use('/api/login', loginRouter);
 
 app.use(middleware.errorHandler);
 
